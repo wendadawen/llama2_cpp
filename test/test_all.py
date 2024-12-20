a7f0fbfd43ff08d1fa816f1aafc606e1c1e77eb5
wendadawen <wendadawen@qq.com>
2024-12-20 13:40:50 +0800

    rearrange：重排文件目录

--- test/test_all.py
+++ test/test_all.py
@@ -3,17 +3,7 @@ Run simply with
 $ pytest
 """
 import os
-import pytest # pip install pytest
-import requests
 import subprocess
-
-
-import torch
-from tokenizer import Tokenizer
-
-# -----------------------------------------------------------------------------
-# test utilities
-
 
 expected_stdout = b'Once upon a time, there was a little girl named Lily. She loved to play outside in the park. One day, she saw a big, red ball. She wanted to play with it, but it was too high.\nLily\'s mom said, "Lily, let\'s go to the park." Lily was sad and didn\'t know what to do. She said, "I want to play with your ball, but I can\'t find it."\nLily was sad and didn\'t know what to do. She said, "I\'m sorry, Lily. I didn\'t know what to do."\nLily didn\'t want to help her mom, so she'
 
@@ -25,9 +15,9 @@ def test_main():
 
     model_path = os.path.join("stories260K.bin")
     tokenizer_path = os.path.join("tok512.bin")
-    command = ["./main.exe", model_path, "-z", tokenizer_path, "-t", "0.0", "-n", "200"]
-    with open('err.txt', mode='wb') as fe:
-        with open('stdout.txt', mode='wb') as fo:
+    command = ["./go.exe", model_path, "-z", tokenizer_path, "-t", "0.0", "-n", "200"]
+    with open('./test/stderr.txt', mode='wb') as fe:
+        with open('./test/stdout.txt', mode='wb') as fo:
             proc = subprocess.Popen(command, stdout=fo, stderr=fe)  #pipe in windows terminal does funny things like replacing \n with \r\n
             proc.wait()
 
